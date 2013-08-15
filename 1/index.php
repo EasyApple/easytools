141682783161570fe0f658878f58d63df0f7a25c
imagispace@sina.com <imagispace@sina.com@086faeb0-d351-4625-b962-ac8c6631c7dd>
2013-08-15 07:52:46 +0000

    添加 VLC插件页

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,13 @@
-<?php
-echo '<strong>Welcome to SAE!</strong>';
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<form action="index.php" method="search">
+    <div id="title">
+        <title>EasyTools</title>
+        <br><center><lable><font type="黑体" size="16" color="blue">灵感工具集</font></lable></center></br>
+    </div>
+    <div id="tools">
+        <center>
+		<a href="vlc.html">VLC插件</a>
+        <br><input value="http://" NAME="myurl" TYPE="text" SIZE="32" onmouseover="this.style.borderColor='blue';this.style.backgroundColor='green'" style="width: 256; height: 32" onmouseout="this.style.borderColor='blue';this.style.backgroundColor='#ffffff'" style="border-width:1px;border-color=blue"></br> 
+        </center>
+    </div>
+</form>
